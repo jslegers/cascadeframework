44b398a8f3df09c0f7dc346730f542f9ec6abcc8
John Slegers <slegersjohn@gmail.com>
2013-03-21 21:44:16 +0100

    JS fix

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,22 +2,22 @@ Detector.load=function(){
     Loader.apply(window,[].slice.call(arguments,0));
 }
 Loader([ {
-    load: 'assets/js/lib/google/prettify/prettify.js',
+    load: App.jsroot + 'lib/google/prettify/prettify.js',
     callback: function () {
         App.log("Prettify loaded!");
     }
 }, {
-    load:  "assets/js/lib/jquery/jquery-" + Detector.jqueryversion + ".js",
+    load:  App.jsroot + 'lib/jquery/jquery-' + Detector.jqueryversion + ".js",
     callback: function () {
         App.log("jQuery loaded!");
     }
 }, {
-    load: 'assets/js/lib/jquery/jquery.easing.js',
+    load: App.jsroot + 'lib/jquery/jquery.easing.js',
     callback: function () {
         App.log("jQuery Easing loaded!");
     }
 }, {
-    load: 'assets/js/lib/jquery/jquery.cascade.js',
+    load: App.jsroot + 'lib/jquery/jquery.cascade.js',
     callback: function () {
         App.log("Cascade loaded!");
     }
