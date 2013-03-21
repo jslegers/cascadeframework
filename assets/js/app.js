4088487c85375b6b32f96615f34527c1f1877b46
John Slegers <slegersjohn@gmail.com>
2013-03-21 22:49:14 +0100

    test

--- assets/js/app.js
+++ assets/js/app.js
@@ -115,6 +115,8 @@ window.App = (function( window, document ) {
             App.rootpath += '/' + App.path[x];
     }
     
+    alert(App.rootpath); 
+    
     App.jsroot = App.rootpath + App.jsroot;
     
     if(App.path[App.path.length-1] == '' || App.path[App.path.length-1] == root){
@@ -186,6 +188,4 @@ window.App = (function( window, document ) {
         }
         ]);
     });
-    
-    console.log(App.rootpath); 
 }());
